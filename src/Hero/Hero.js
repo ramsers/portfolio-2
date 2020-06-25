bc6443d34e4ae797d22b3931872d14d38078cac0
Raheim <raheimbbailey@gmail.com>
2020-06-25 05:02:11 -0400

    position nav bar in place-mobile, add hover effect

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Hero.scss';
+import Nav from '../Nav/Nav';
 
 const Hero = () => {
     return(
         <section className="hero">
             <section className="hero__ctn">
-                
+                <Nav/>
             </section>
         </section>
     )
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import './Nav.scss';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
+import {BrowserRouter} from 'react-router-dom';
 
 const Nav =() => {
     return(
         <nav className="site-nav">
             <div className="site-nav__ctn">
-                <NavLink to='/#home'>Home</NavLink>
-                <NavLink to='/#projects'>Projects</NavLink>
-                <NavLink to='/#about'>About</NavLink>
-                <NavLink to='/#contact'>Contact</NavLink>
+                <BrowserRouter>
+                    <NavLink className="site-nav__link" to='/#home'>
+                        Home
+                        <span className="site-nav__link-box"></span>
+                    </NavLink>
+
+                    <NavLink className="site-nav__link" to='/#projects'>
+                        Projects
+                        <span className="site-nav__link-box"></span>
+                    </NavLink>
+
+                    <NavLink className="site-nav__link" to='/#about'>
+                        About
+                        <span className="site-nav__link-box"></span>
+                    </NavLink>
+
+                    <NavLink className="site-nav__link" to='/#contact'>
+                        Contact
+                        <span className="site-nav__link-box"></span>
+                    </NavLink>
+                </BrowserRouter>
             </div>
         </nav>
     )
