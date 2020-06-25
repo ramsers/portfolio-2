22d6c3135273be3ec54095502343459f88ed8a57
Raheim <raheimbbailey@gmail.com>
2020-06-25 02:33:43 -0400

    add hero component & base sass utilities

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header =() => {
+    return(
+        <header className="head">
+            <div className="head__ctn">
+                
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Portfolio/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+    return(
+        <section className="hero">
+            <section className="hero__ctn">
+                
+            </section>
+        </section>
+    )
+}
+
+export default Hero;
