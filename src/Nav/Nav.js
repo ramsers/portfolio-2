5efb58aadb35c474a4ee9266b92c2fb026ffab64
Raheim <raheimbbailey@gmail.com>
2020-06-26 00:03:41 -0400

    experiment with header nav styles

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,34 +3,49 @@ import './Nav.scss';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
 import {BrowserRouter} from 'react-router-dom';
 
-const Nav =() => {
-    return(
-        <nav className="site-nav">
-            <div className="site-nav__ctn">
-                <BrowserRouter>
-                    <NavLink className="site-nav__link" to='/#home'>
-                        Home
-                        <span className="site-nav__link-box"></span>
-                    </NavLink>
+class Nav extends React.Component {
+    state = {
+        actives: false
+    }
 
-                    <NavLink className="site-nav__link" to='/#projects'>
-                        Projects
-                        <span className="site-nav__link-box"></span>
-                    </NavLink>
+    toggle = () => {
+        let links = document.getElementsByClassName("site-nav__link-box")
+        console.log(links)
 
-                    <NavLink className="site-nav__link" to='/#about'>
-                        About
-                        <span className="site-nav__link-box"></span>
-                    </NavLink>
+        // this.setState({
+        //     actives: !this.state.actives
+        // })
+    }
 
-                    <NavLink className="site-nav__link" to='/#contact'>
-                        Contact
-                        <span className="site-nav__link-box"></span>
-                    </NavLink>
-                </BrowserRouter>
-            </div>
-        </nav>
-    )
+    render() {
+        return(
+            <nav className="site-nav">
+                <div className="site-nav__ctn">
+                    <BrowserRouter>
+                        <NavLink activeClassName="active" className="site-nav__link" to='/#home'>
+                            Home
+                            <span className='site-nav__link-box'></span>
+                        </NavLink>
+    
+                        <NavLink  className="site-nav__link" to='/#projects'>
+                            Projects
+                            <span className='site-nav__link-box'></span>
+                        </NavLink>
+    
+                        <NavLink  className="site-nav__link" className="site-nav__link" to='/#about'>
+                            About
+                            <span className='site-nav__link-box'></span>
+                        </NavLink>
+    
+                        <NavLink  className="site-nav__link" to='/#contact'>
+                            Contact
+                            <span className='site-nav__link-box'></span>
+                        </NavLink>
+                    </BrowserRouter>
+                </div>
+            </nav>
+        )
+    }
 }
 
 export default Nav;
