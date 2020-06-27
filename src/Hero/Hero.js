35c174a95e736980e0dfd2cb8d87b1703c5c8529
Raheim <raheimbbailey@gmail.com>
2020-06-27 15:51:49 -0400

    add mobile hero styles, begin tablet styles

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -4,11 +4,21 @@ import Nav from '../Nav/Nav';
 
 const Hero = () => {
     return(
+        <>
         <section className="hero">
+            <Nav/>
             <section className="hero__ctn">
-                <Nav/>
+                <div className="hero__content-ctn">
+                    <div className="hero__circle"></div>
+                    <div className="hero__content">
+                        <h1 className="hero__head">Web Developer</h1>
+                        <h2 className="hero__sub-head">Front End Focused</h2>
+                        <h2 className="hero__sub-head">Cake Enthusiast</h2>
+                    </div>
+                </div>
             </section>
         </section>
+        </>
     )
 }
 
