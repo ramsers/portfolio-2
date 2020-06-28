597807514823a9a8364d9688c72b78c90ff08dbf
Raheim <raheimbbailey@gmail.com>
2020-06-28 03:12:56 -0400

    change nav & circle color, begin adding animations

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import './Hero.scss';
 import Nav from '../Nav/Nav';
+import {TweenMax, Power3} from 'gsap';
 
 const Hero = () => {
+
+    let heroShape = useRef(null);
+    let heroContent = useRef(null);
+
+    useEffect(()=> {
+        TweenMax.to(
+            heroShape,
+            2,
+            {
+                opacity:1,
+                y: 0,
+                ease:Power3.easeOut
+            }
+        )
+        TweenMax.to(
+            heroContent,
+            2, 
+            {
+                opacity:1,
+                x: 0,
+                ease: Power3.easeOut
+            }
+        )
+    }, [])
+
     return(
         <>
         <section className="hero">
             <Nav/>
             <section className="hero__ctn">
                 <div className="hero__content-ctn">
-                    <div className="hero__circle"></div>
-                    <div className="hero__content">
+                    <div ref={el => {heroShape = el}} className="hero__circle"></div>
+                    <div ref={el=>{heroContent = el}} className="hero__content">
                         <h1 className="hero__head">Web Developer</h1>
                         <h2 className="hero__sub-head">Front End Focused</h2>
                         <h2 className="hero__sub-head">Cake Enthusiast</h2>
