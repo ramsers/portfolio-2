6a33843016e3c0e0e88b7632cd85201d0bb424d6
Raheim <raheimbbailey@gmail.com>
2020-07-04 00:57:48 -0400

    add contact mobile styling

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -11,23 +11,33 @@ const Contact = () => {
         <section className="contact">
             <div className="contact__ctn">
                 <div className="contact__social-ctn">
-                    <div className="contact__social">
-                        <img className="contact__icon" src={emailIcon} alt=""/>
-                    </div>
-                    <div className="contact__social">
-                        <img className="contact__icon" src={gitHubIcon} alt=""/>
+                    <div className="contact__social-head">
+                        <h3 className="contact__social-title">Like What You See?</h3>
+                        <p className="contact__social-text">Contact Me Here</p>
                     </div>
                     <div className="contact__social">
                         <img className="contact__icon" src={linkdIcon} alt=""/>
-                    </div>  
+                        <p className="contact__url">linkedin.com/in/raheim-bailey</p>
+                    </div>
+                    <div className="contact__social">
+                        <img className="contact__icon" src={emailIcon} alt=""/>
+                        <p className="contact__url">raheimbbailey@gmail.com</p>
+                    </div> 
+                    <div className="contact__social">
+                        <img className="contact__icon" src={gitHubIcon} alt=""/>
+                        <p className="contact__url">github.com/ramsers</p>
+                    </div> 
                 </div>
 
                 <div className="contact__commit">
-                    <h4 className="contact__commit-title"></h4>
-                    <p className="contact__commit-msg"></p>
+                    <h4 className="contact__commit-title">My Commitment</h4>
+                    <p className="contact__commit-msg">To bring as much value to any organization that I am
+                        apart of and to conitually learn and improve my skills as a developer.
+                    </p>
                 </div>
                 
                 <div className="contact__navigate">
+                    <h4 className="contact__navigate-title">Navigate</h4>
                     <BrowserRouter>
                         <NavLink className="contact__link" to='/#projects'>Portfolio</NavLink>
                     </BrowserRouter>
